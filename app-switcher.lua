797ea0d719f7772486787797a97d3091afc6306a
Will Fairclough <github@willdoes.dev>
2025-01-13 13:00:43 -0500

    Added Notion, Linear, Slack and Messages to app-switcher

--- app-switcher.lua
+++ app-switcher.lua
@@ -5,12 +5,16 @@ local appLauncher = hs.loadSpoon("AppLauncher") or error("AppLauncher spoon coul
 local mappings = {
 	B = "Google Chrome",
 	F = "Finder",
-	S = "Safari",
 	H = "Hammerspoon",
+	N = "Notion",
+	L = "Linear",
+	M = "Messages",
+	S = "Slack",
 	T = "Alacritty",
 	V = "Visual Studio Code",
 	W = "Windows 11",
 	X = "Xcode",
+	Z = "Zoom.us",
 }
 mappings["1"] = "1Password"
 mappings["A"] = "Arc"
